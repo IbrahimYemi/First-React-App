d79395965950ee49e609b9a6f51a4c34179e6dff
IbrahimYemi <sharafadeen.might@gmail.com>
2022-06-01 02:05:14 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './style.css';
+import Forms from './Forms';
+import Header from './Header';
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Forms />
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-xquxuk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-xquxuk)
